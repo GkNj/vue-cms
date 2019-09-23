dac795ad0fbd42d9682a9ec7d4b5ba6193ce0979
GkNj <846558172@qq.com>
2019-09-23 16:23:33 +0800

    项目结构搭建成功

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" class="app-container">
+        <!--顶部的header区域-->
+        <mt-header fixed title="凯哥·vue项目"></mt-header>
+        <br>
+        <h1>这是一个app组件</h1>
+        <!--        中间路由的 router-view 区域-->
+
+
+        <!--        底部tabbar区域-->
+        <nav class="mui-bar mui-bar-tab">
+            <a class="mui-tab-item mui-active" href="#tabbar">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-chat">
+                <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+                <span class="mui-tab-label">消息</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-contact">
+                <span class="mui-icon mui-icon-contact"></span>
+                <span class="mui-tab-label">通讯录</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-map">
+                <span class="mui-icon mui-icon-gear"></span>
+                <span class="mui-tab-label">设置</span>
+            </a>
+        </nav>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+
+    }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+    .app-container {
+        padding-top: 40px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+//入口文件
+
 import Vue from 'vue'
 import App from './App.vue'
+//导入mint-ui中的组件
+import MintUI from 'mint-ui';
+import 'mint-ui/lib/style.css'
 
+Vue.use(MintUI)
 Vue.config.productionTip = false
+//导入mui组件
+import './lib/mui/css/mui.min.css'
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
